380d4924f3ed86dba7e3a622bae459d7871293db
Andy Boothe <andy.boothe@gmail.com>
2023-11-01 10:36:47 -0500

    bump credential retrieval logic

--- src/main/java/io/humangraphics/backend/lambda/ContainerAwsSigningCredentialsProvider.java
+++ src/main/java/io/humangraphics/backend/lambda/ContainerAwsSigningCredentialsProvider.java
@@ -1,13 +1,16 @@
 package io.humangraphics.backend.lambda;
 
+import static java.lang.String.format;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import org.json.JSONObject;
 import com.sigpwned.httpmodel.aws.AwsSigningCredentials;
 import com.sigpwned.httpmodel.aws.AwsSigningCredentialsProvider;
+import io.humangraphics.backend.lambda.util.ByteStreams;
 
 /**
  * Fetches credentials from the endpoint in environment variable AWS_CONTAINER_CREDENTIALS_FULL_URI.
@@ -29,15 +32,38 @@ public class ContainerAwsSigningCredentialsProvider implements AwsSigningCredent
           .orElseThrow(() -> new AssertionError(
               "No value for environment variable AWS_CONTAINER_CREDENTIALS_FULL_URI"));
 
+  private static final String AWS_CONTAINER_AUTHORIZATION_TOKEN =
+      Optional.ofNullable(System.getenv("AWS_CONTAINER_AUTHORIZATION_TOKEN"))
+          .orElseThrow(() -> new AssertionError(
+              "No value for environment variable AWS_CONTAINER_AUTHORIZATION_TOKEN"));
+
   /**
    * https://github.com/aws/aws-sdk-java/blob/master/aws-java-sdk-core/src/main/java/com/amazonaws/auth/BaseCredentialsFetcher.java#L55C1-L62C1
+   * https://github.com/aws/aws-sdk-java/blob/master/aws-java-sdk-core/src/main/java/com/amazonaws/internal/EC2ResourceFetcher.java#L38
+   * https://github.com/aws/aws-sdk-java/blob/master/aws-java-sdk-core/src/main/java/com/amazonaws/internal/ConnectionUtils.java#L78
+   * https://github.com/aws/aws-lambda-runtime-interface-emulator/blob/bf7e2486034742b84a1a25d28478e147b5e65f06/lambda/rapi/handler/credentials.go#L20
    */
   private AwsSigningCredentials fetchCredentials() {
     JSONObject response;
-    try (InputStream in = new URL(AWS_CONTAINER_CREDENTIALS_FULL_URI).openStream()) {
-      response = new JSONObject(in);
+
+    try {
+      HttpURLConnection cn =
+          (HttpURLConnection) new URL(AWS_CONTAINER_CREDENTIALS_FULL_URI).openConnection();
+      try {
+        cn.setRequestProperty("authorization", AWS_CONTAINER_AUTHORIZATION_TOKEN);
+        if (cn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+          response = new JSONObject(cn.getInputStream());
+        } else {
+          String errorBody =
+              new String(ByteStreams.toByteArray(cn.getErrorStream()), StandardCharsets.UTF_8);
+          throw new IOException(
+              format("Request failed; status=%d; body=%s", cn.getResponseCode(), errorBody));
+        }
+      } finally {
+        cn.disconnect();
+      }
     } catch (IOException e) {
-      throw new UncheckedIOException("Failed to retrieve credentials", e);
+      throw new UncheckedIOException("Failed to retrieve container credentials", e);
     }
 
     String accessKeyId = response.getString("AccessKeyId");
